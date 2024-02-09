9e4b3169ef1df4b0fb954f5a2db7458895cedb30
david <david.gmzherrera28@gmail.com>
2024-02-09 00:40:16 -0600

    Initial Commit

---
+++ proyecto01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proyecto01
+//
+//  Created by david gomez herrera on 21/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
